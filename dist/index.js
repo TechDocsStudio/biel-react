d1f7d0a641b1d38c242711aee6d95348b70f6377
David Garcia <hi@davidgarcia.dev>
2024-11-12 16:23:17 +0000

    chore: bump version

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
 import { createReactComponent } from './react-component-lib';
 export const BielButton = createReactComponent('biel-button');
+export const BielSearchButton = createReactComponent('biel-search-button');
 //# sourceMappingURL=index.js.map
--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,1 +1,2 @@
 export declare const BielButton: import("react").ForwardRefExoticComponent<import("react").RefAttributes<unknown>>;
+export declare const BielSearchButton: import("react").ForwardRefExoticComponent<import("react").RefAttributes<unknown>>;
